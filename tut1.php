91031884e4e6934ba5212575ebc21558bbe441c4
RajAnubhav <anubhavraj742@gmail.com>
2021-12-05 19:05:16 +0530

    sample_example1

---
+++ tut1.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP website</title>
+</head>
+<body>
+    <h1>Welcome to my website.</h1>
+    <div>
+        this is a tutorial 
+    </div>
+    <?php
+    
+        echo "This is tutorial 1 ";
+        // <br>
+        // keywords are not sensitive - in php
+        // variables are containers for storing information
+        // starts with a $
+        $name = "Anubhav";
+        $income = 4000;
+        echo "This guy is $name  and his income is $income";
+
+    ?>
+</body>
+</html>
