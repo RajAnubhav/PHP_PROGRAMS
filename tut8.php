5057bcba3ea047014d1b427e46a4421ca4aaa80a
RajAnubhav <anubhavraj742@gmail.com>
2021-12-06 19:49:01 +0530

    loops

---
+++ tut8.php
@@ -0,0 +1,16 @@
+<?php
+// loops in php
+echo "loops in php<br>";
+// echo 1;echo "<br>";
+// echo 2;echo "<br>";
+// echo 3;echo "<br>";
+// echo 4;echo "<br>";
+// echo 5;echo "<br>";
+$i = 0;
+while($i<11){
+    echo $i+1;
+    echo "<br>";
+    $i++;
+}
+
+?>
