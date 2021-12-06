f3514298a0513755bc2deb2f714f511284be0986
RajAnubhav <anubhavraj742@gmail.com>
2021-12-06 19:43:51 +0530

    operators

---
+++ tut5.php
@@ -0,0 +1,16 @@
+<!--operators in php -->
+<?php 
+    $a = 15;
+    $b = 25;
+    echo "Sum of a and b is ".($a+$b);
+    echo "<br>";
+    echo "Difference of a and b is ".($a-$b);
+    echo "<br>";
+    echo "Multipication of a and b is ".($a*$b);
+    echo "<br>";
+    echo "Division of a and b is ".($a/$b);
+    echo "<br>";
+    echo "Modulus of a and b is ".($a%$b);
+    echo "<br>";
+    // ! is used for the negation
+?>
