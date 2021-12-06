4ed1d3eb259ae6ff37f17ca4b2aff56689270d4a
RajAnubhav <anubhavraj742@gmail.com>
2021-12-06 19:48:48 +0530

    switch

---
+++ tut7.php
@@ -0,0 +1,18 @@
+<?php
+    // switch statement in php
+$age = 12;
+    switch($age){
+        case '12':
+            echo "You are 12 years old";
+            break;
+        case 42:
+            echo "You are 42 years old";
+            break;
+        case 23:
+            echo "You are 23 years old";
+            break;
+        default:
+            echo "Your age is not present";
+            break;
+        }
+?>
